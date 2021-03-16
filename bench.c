e751493bed8fa5db24f2e72bbaab4db50bbf8bb7
Omkar S. Dubas <osd14@ilab4.cs.rutgers.edu>
2021-03-16 13:08:19 -0400

    bench file changes

--- bench.c
+++ bench.c
@@ -12,29 +12,31 @@ pthread_mutex_t mutex;
  * @return 	null
  */
 void busyWait(int i) {
-	int j = 21474;
-	i = i < 0 ? 1 : i;
-	while (i>=0) {
-		while (j>=0) {j--;}
-		i--;
-	}
+	int j = 21474;//21474
+    i = i < 0 ? 1 : i;
+    int k = j*j;
+    while (k>0) {
+        // if(k%10000000 == 0) {printf("Inside Thread -> %d with value k=%d\n",i,k);
+        // sleep(5);
+        // while (j>=0) {j--;}
+        // i--;
+        k--;
+    }
 }
 /***
  * funThread1 - Function for thread 1. This is the function that is executed when thread 1 is scheduled.
  * @param 	null
  * @return 	null
  */
-void *funThread1() {
-	printf("Thread  1 trying to lock the mutex now\n");
+void *thread1() {
+	printf("Thread 1 is trying to lock the mutex\n");
     pthread_mutex_lock(&mutex);
-    printf("Thread  1 sucessfully locked the mutex!!\n");
     int i;
-    for(i = 0; i < 10; i++){
+    for(i = 0; i < 2; i++){
         busyWait(1);
+        pthread_mutex_unlock(&mutex);
         printf("This is the first Thread 1\n");
-     }
-    pthread_mutex_unlock(&mutex);
-    printf("Thread  1 unlocked the mutex\n");
+    }
 }
 
 /***
@@ -42,19 +44,18 @@ void *funThread1() {
  * @param 	null
  * @return 	null
  */
-void *funThread2() {
-	printf("Thread  2 trying to lock the mutex now\n");
+void *thread2() {
+	int i;
+    printf("Thread 2 is trying to lock the mutex \n");
     pthread_mutex_lock(&mutex);
-    printf("Thread  2 sucessfully locked the mutex!!\n");
-    int i;
-    for(i = 0; i < 3 ; i++) {
-        busyWait(1);
+    printf("Thread 2 has successfully acquired the lock\n");
+    for(i = 0; i < 2 ; i++) {
+        busyWait(2);
         printf("This is the second Thread 2\n");
     }
+    printf("Thread 2 is trying to unlock the mutex\n");
     pthread_mutex_unlock(&mutex);
-    printf("Thread  2 unlocked the mutex\n");
     printf("Thread  2 EXITING!!!!!!!!\n");
-    pthread_exit(&i);
 }
 
 /***
@@ -62,18 +63,14 @@ void *funThread2() {
  * @param 	null
  * @return 	null
  */
-void *funThread3() {
+void *thread3() {
     int i;
-    long j;
-    for(i = 0; i < 2 ; i++) {
-        busyWait(1);
+
+    for(i = 0; i < 3 ; i++) {
+        busyWait(3);
         printf("This is the third Thread 3\n");
     }
-    for(i = 0; i < 4 ; i++) {
-		for(j=0;j<1000000000;j++){}
-        // pthread_yield();
-		printf("Thread 3 YIELDED!!\n");
-    }
+
     printf("Thread  3 is done!\n");
 }
 /***
@@ -81,35 +78,30 @@ void *funThread3() {
  * @param 	null
  * @return 	null
  */
-void *funThread4() {
-	printf("Thread  4 trying to lock the mutex now\n");
-    pthread_mutex_lock(&mutex);
-    printf("Thread  4 sucessfully locked the mutex!!\n");
-    int i;
-    for(i = 0; i < 4 ; i++) {
-        busyWait(1);
+void *thread4() {
+	int i;
+    for(i = 0; i < 3 ; i++) {
+        busyWait(4);
         printf("This is the fourth Thread 4\n");
     }
-    pthread_mutex_unlock(&mutex);
-    printf("Thread  4 unlocked the mutex\n");
 }
 
 int main(int argc, const char * argv[]) {
 	struct timeval start, end;
 	float delta;
 	gettimeofday(&start, NULL);
-	pthread_t thread1,thread2,thread3,thread4;
+	pthread_t t1,t2,t3,t4;
     pthread_mutex_init(&mutex, NULL);
     //Create threads
-    pthread_create(&thread1, NULL, &funThread1,NULL);
-    pthread_create(&thread2, NULL, &funThread2,NULL);
-    pthread_create(&thread3, NULL, &funThread3,NULL);
-    pthread_create(&thread4, NULL, &funThread4,NULL);
+    pthread_create(&t1, NULL, &thread1,NULL);
+    pthread_create(&t2, NULL, &thread2,NULL);
+    pthread_create(&t3, NULL, &thread3,NULL);
+    pthread_create(&t4, NULL, &thread4,NULL);
     //Call join on the threads
-    pthread_join(thread1,NULL);
-    pthread_join(thread2,NULL);
-    pthread_join(thread3,NULL);
-    pthread_join(thread4,NULL);
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+    pthread_join(t4,NULL);
     //Destroying the mutex
     pthread_mutex_destroy(&mutex);
     gettimeofday(&end, NULL);
@@ -118,3 +110,4 @@ int main(int argc, const char * argv[]) {
     printf("Ending main!\n");
     return 0;
 }
+
